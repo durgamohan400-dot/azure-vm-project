fe9d8ce98216c0b01f57de20b50a004d9c4e381c
Ubuntu <azureuser@vm-win-01.zwf3vs0vgmeulhab30trwexhsc.rx.internal.cloudapp.net>
2026-04-02 08:33:57 +0000

    Added deploy script for CI/CD

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,13 @@
 git pull origin main
 sudo cp index.html /var/www/html/index.html
 sudo systemctl restart nginx
+
+#!/bin/bash
+set -e
+
+cd ~/azure-vm-project
+git pull origin main
+sudo docker stop my-web-app || true
+sudo docker rm my-web-app || true
+sudo docker build -t my-web-app .
+sudo docker run -d --name my-web-app -p 80:80 my-web-app
